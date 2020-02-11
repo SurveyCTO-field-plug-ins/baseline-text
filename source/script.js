90f5a3c7b0487870f32a09a29e95c8eb49192682
Chuck King <cking@surveycto.com>
2020-02-11 17:27:44 -0500

    Add support for basic appearance options

--- source/script.js
+++ source/script.js
@@ -20,3 +20,13 @@ function setFocus() {
 input.oninput = function() {
     setAnswer(input.value);
 }
+
+// check for standard appearance options and apply them
+if ( fieldProperties.APPEARANCE.includes("numbers_phone") == true ) {
+    input.type = "tel";
+} else if ( fieldProperties.APPEARANCE.includes("numbers_decimal") == true ) {
+    input.pattern = "[0-9]*";
+    input.inputmode = "numeric";
+} else if ( fieldProperties.APPEARANCE.includes("numbers") == true ) {
+    input.type = "number";
+} 
